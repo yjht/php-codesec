60927022a1dc03829849561829f073d14396421e
Yvan JANET <73112780+yjht@users.noreply.github.com>
2022-01-28 18:57:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 const $LOGIN_EXEPECTED_FORMAT = /^[a-z]{8}[a-z0-9]{4}$/;
 const $PASSW_EXEPECTED_FORMAT = /^.{12,}$/;
 const $CONTENT_EXPECTED_FORMAT = //;
-const $BACK_URL = 'https://".$_SERVER['HTTP_HOST']."/back.php;
+const $BACK_URL = 'https://'.$_SERVER['HTTP_HOST'].'/back.php';
 
 if (isset($_POST['login'])) {
   $login = $_POST['login'];
   $passw = $_POST['password'];
   $content= $_POST['content'];
-
 
   if (preg_match($LOGIN_EXEPECTED_FORMAT, $login) !== 1 ) $login = "";
   if (preg_match($PASSW_EXEPECTED_FORMAT, $passw) !== 1 ) $passw = "";
